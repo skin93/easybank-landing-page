70dd015f1b7419b7372458b03dd9c02419df5758
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2022-02-06 20:23:27 +0100

    Add TheFooter

---
+++ src/containers/TheFooter/imports.js
@@ -0,0 +1,7 @@
+import facebook from "/src/assets/images/icon-facebook.svg";
+import youtube from "/src/assets/images/icon-youtube.svg";
+import twitter from "/src/assets/images/icon-twitter.svg";
+import pinterest from "/src/assets/images/icon-pinterest.svg";
+import instagram from "/src/assets/images/icon-instagram.svg";
+
+export { facebook, youtube, twitter, pinterest, instagram };
--- src/containers/TheFooter/index.jsx
+++ src/containers/TheFooter/index.jsx
@@ -1,5 +1,46 @@
+import "./TheFooter.css";
+import { Button } from "/src/components";
+import { facebook, youtube, twitter, pinterest, instagram } from "./imports";
+
 const TheFooter = () => {
-  return <footer>The Footer</footer>;
+  return (
+    <footer className="footer section-padding">
+      <img
+        className="footer__logo"
+        src="/src/assets/images/logo.svg"
+        alt="Logo"
+      />
+      <div className="footer__social-medias">
+        <img className="footer__social-media" src={facebook} alt="Facebook" />
+        <img className="footer__social-media" src={youtube} alt="YouTube" />
+        <img className="footer__social-media" src={twitter} alt="Twitter" />
+        <img className="footer__social-media" src={pinterest} alt="Pinterest" />
+        <img className="footer__social-media" src={instagram} alt="Instagram" />
+      </div>
+      <ul className="footer__links">
+        <li className="footer__link">
+          <a href="#"></a>About Us
+        </li>
+        <li className="footer__link">
+          <a href="#"></a>Contact
+        </li>
+        <li className="footer__link">
+          <a href="#"></a>Blog
+        </li>
+        <li className="footer__link">
+          <a href="#"></a>Careers
+        </li>
+        <li className="footer__link">
+          <a href="#"></a>Support
+        </li>
+        <li className="footer__link">
+          <a href="#"></a>Privacy Policy
+        </li>
+      </ul>
+      <Button />
+      <p className="footer__copy"> © Easybank. All Rights Reserved</p>
+    </footer>
+  );
 };
 
 export default TheFooter;
