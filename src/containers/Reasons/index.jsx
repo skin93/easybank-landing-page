e972bd98a94ea5a8bc9526780c8b142d6e8cd358
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2022-02-06 21:28:57 +0100

    Style articles & reasons sections on desktop

--- src/containers/Reasons/index.jsx
+++ src/containers/Reasons/index.jsx
@@ -5,11 +5,13 @@ import { Reason } from "../../components";
 const Reasons = () => {
   return (
     <section className="reasons section-padding">
-      <h1 className="reasons__title">Why choose Easybank?</h1>
-      <p className="reasons__info">
-        We leverage Open Banking to turn your bank account into your financial
-        hub. Control your finances like never before.
-      </p>
+      <div className="reasons__intro">
+        <h1 className="reasons__title">Why choose Easybank?</h1>
+        <p className="reasons__info">
+          We leverage Open Banking to turn your bank account into your financial
+          hub. Control your finances like never before.
+        </p>
+      </div>
       <div className="reasons__list">
         {reasons.map((reason) => (
           <Reason
