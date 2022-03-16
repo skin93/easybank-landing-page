551c3a95533c1c129092b3d6c7aee778c57d6dfd
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2022-03-16 17:32:12 +0100

    Fix imports

--- src/containers/Hero/index.jsx
+++ src/containers/Hero/index.jsx
@@ -1,14 +1,11 @@
+import phones from "/src/assets/images/image-mockups.png";
 import "./Hero.css";
 import { Button } from "../../components";
 
 const Hero = () => {
   return (
     <section className="hero">
-      <img
-        className="hero__img-phones"
-        src="/src/assets/images/image-mockups.png"
-        alt="bg-intro"
-      />
+      <img className="hero__img-phones" src={phones} alt="bg-intro" />
       <div className="hero__img-background" />
       <div className="hero__content section-padding">
         <h1 className="hero__title">Next generation digital banking</h1>
--- src/containers/TheHeader/index.jsx
+++ src/containers/TheHeader/index.jsx
@@ -1,7 +1,9 @@
 import { useContext } from "react";
-
-import { Button, Nav, Logo } from "../../components";
 import { AppContext } from "../../context/AppContext";
+
+import hamburger from "/src/assets/images/icon-hamburger.svg";
+import close from "/src/assets/images/icon-close.svg";
+import { Button, Nav, Logo } from "../../components";
 
 import "./TheHeader.css";
 
@@ -15,14 +17,14 @@ const TheHeader = () => {
       <Button />
       {toggle ? (
         <img
-          src="src/assets/images/icon-close.svg"
+          src={close}
           className="header__icon-close"
           alt="Close"
           onClick={() => dispatch({ type: "SET_TOGGLE" })}
         />
       ) : (
         <img
-          src="src/assets/images/icon-hamburger.svg"
+          src={hamburger}
           className="header__icon-hamburger"
           alt="Hamburger"
           onClick={() => dispatch({ type: "SET_TOGGLE" })}
